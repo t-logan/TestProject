fc1dd875c95b4e5f5d0be4d29efdd827276ef9e0
t-logan <timothy.d.logan@gmail.com>
2014-01-20 19:26:17 -0800

    writeHDFFile now processes multiple input CSV files.

--- HDF5/writeHDF5File.py
+++ HDF5/writeHDF5File.py
@@ -4,18 +4,44 @@ import sys
 import h5py
 import Image
 import numpy
+import os
 
-linesIn = 0
+class DirectoryWalker:
+# a forward iterator that traverses a directory tree
 
+  def __init__(self, directory):
+    self.stack = [directory]
+    self.files = []
+    self.index = 0
+
+  def __getitem__(self, index):
+    while 1:
+        try:
+            file = self.files[self.index]
+            self.index = self.index + 1
+        except IndexError:
+            # pop next directory from stack
+            self.directory = self.stack.pop()
+            self.files = os.listdir(self.directory)
+            self.index = 0
+        else:
+            # got a filename
+            fullname = os.path.join(self.directory, file)
+            if os.path.isdir(fullname) and not os.path.islink(fullname):
+                self.stack.append(fullname)
+            return fullname
+        
 if len(sys.argv) != 4:                                      # check number of command line args
     print "Argument error, need: <output directory> <image file name> <CSV file name>. Quitting."
     raise SystemExit(1)
 else:
     print "Output directory: " + sys.argv[1]                # display output directory name                             
     print "Image file: " + sys.argv[2]                      # display image file name                             
-    print "Input CSV file: " + sys.argv[3]                  # display CSV file name                             
-
-for line in  open(sys.argv[3]):                             # read the CSV file
+    print "Input CSV file: " + sys.argv[3]                  # display CSV file name 
+        
+def processFile(file):
+  linesIn = 0
+  for line in  open(file):                             # read the CSV file
     linesIn += 1
     fields = line.split(",")                                # tokenize the input
     if(fields[0] != '""' and linesIn != 1):
@@ -42,5 +68,9 @@ for line in  open(sys.argv[3]):                             # read the CSV file
     if(linesIn != 1):                                       # skip the header, write the emissions record ...
         ds[emissionPtr:emissionPtr+1] = [(fields[7], float(fields[8]), float(fields[9]), float(fields[10]), float(fields[11]))]
         emissionPtr += 1
+                      
+for file in DirectoryWalker(os.path.abspath('c:/tmp')):
+    if(str.find(file, ".csv") != -1):
+        processFile(file)
         
 print "Done."
