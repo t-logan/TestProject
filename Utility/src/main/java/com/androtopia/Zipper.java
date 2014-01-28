801e085044109413adf789cb7961a937d4522e54
t-logan <timothy.d.logan@gmail.com>
2014-01-27 22:00:22 -0800

    updated Zipper class to support unzip

--- Utility/src/main/java/com/androtopia/Zipper.java
+++ Utility/src/main/java/com/androtopia/Zipper.java
@@ -40,40 +40,32 @@ public class Zipper {
 			e.printStackTrace();
 		}
 	}
-	
-	// TODO: unzip
-	 public static void main (String argv[]) {
-	      try {
-	         final int BUFFER = 2048;
-	         BufferedOutputStream dest = null;
-	         FileInputStream fis = new 
-		   FileInputStream(argv[0]);
-	         CheckedInputStream checksum = new 
-	           CheckedInputStream(fis, new Adler32());
-	         ZipInputStream zis = new 
-	           ZipInputStream(new 
-	             BufferedInputStream(checksum));
-	         ZipEntry entry;
-	         while((entry = zis.getNextEntry()) != null) {
-	            System.out.println("Extracting: " +entry);
-	            int count;
-	            byte data[] = new byte[BUFFER];
-	            // write the files to the disk
-	            FileOutputStream fos = new 
-	              FileOutputStream(entry.getName());
-	            dest = new BufferedOutputStream(fos, 
-	              BUFFER);
-	            while ((count = zis.read(data, 0, 
-	              BUFFER)) != -1) {
-	               dest.write(data, 0, count);
-	            }
-	            dest.flush();
-	            dest.close();
-	         }
-	         zis.close();
-	         System.out.println("Checksum: "+checksum.getChecksum().getValue());
-	      } catch(Exception e) {
-	         e.printStackTrace();
-	      }
-	   }
+
+	public void unzip(String inputFilename) {
+		try {
+			final int BUFFER = 2048;
+			BufferedOutputStream dest = null;
+			FileInputStream fis = new FileInputStream(inputFilename);
+			CheckedInputStream checksum = new CheckedInputStream(fis,
+					new Adler32());
+			ZipInputStream zis = new ZipInputStream(new BufferedInputStream(
+					checksum));
+			ZipEntry entry;
+			while ((entry = zis.getNextEntry()) != null) {
+				int count;
+				byte data[] = new byte[BUFFER];
+				// write the files to the disk
+				FileOutputStream fos = new FileOutputStream(entry.getName());
+				dest = new BufferedOutputStream(fos, BUFFER);
+				while ((count = zis.read(data, 0, BUFFER)) != -1) {
+					dest.write(data, 0, count);
+				}
+				dest.flush();
+				dest.close();
+			}
+			zis.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 }
