18c6d5b0532e645b2c7972376df20469fadf849c
t-logan <timothy.d.logan@gmail.com>
2014-02-02 15:48:15 -0800

    ReadXMLFiles now uses properties.

--- DLC/src/main/java/com/androtopia/ReadXMLFiles.groovy
+++ DLC/src/main/java/com/androtopia/ReadXMLFiles.groovy
@@ -3,12 +3,14 @@ package com.androtopia;
 import static java.lang.System.out
 
 import java.io.Console;
+import java.io.InputStream;
 import java.nio.charset.Charset
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Properties;
 import java.util.Scanner;
 
 import org.apache.commons.codec.binary.Base64
@@ -22,14 +24,14 @@ import org.apache.commons.io.DirectoryWalker
  */
 public class ReadXMLFiles extends DirectoryWalker {
 
-	private String url = "jdbc:mysql://localhost:3306/DLC"
-	private Connection con = null;
+	private String url = null
+	private Connection con = null
 
-	private String user;
-	private String password;
+	private String user
+	private String password
 
 	private String encodedConverterPic
-	private Zipper zipper = new Zipper();
+	private Zipper zipper = new Zipper()
 	private long xmlReadStartTime = 0;
 	private long xmlReadTime = 0;
 	private long cxmlReadStartTime = 0;
@@ -45,20 +47,22 @@ public class ReadXMLFiles extends DirectoryWalker {
 	 */
 	public static void main(String[] args) {
 		ReadXMLFiles self = new ReadXMLFiles();
-		//		if(args.size() != 2)
-		//			throw new IllegalArgumentException("Must pass input file name and output XML file data path on command line.");
-		//		self.inputCSVFile = args[0];
-		//		self.outputDataPath = args[1];
+		
+		// load properties
+		Properties props = new Properties();
+		ClassLoader cl = GenerateCSVFiles.class.getClassLoader();
+		InputStream is = cl.getResourceAsStream("dlc.properties");
+		props.load(is);
+		is.close();
 
-		Scanner input = new Scanner(System.in)
-		println "Enter User: "
-		self.user = input.nextLine();
-		println "Enter Password: "
-		self.password = input.nextLine();
-		self.con = DriverManager.getConnection(self.url, self.user, self.password);
+		self.url = props.getProperty("db.url");
+		self.user = props.getProperty("db.user");
+		self.password = props.getProperty("db.pw");
+		self.con = DriverManager.getConnection(self.url, self.user,
+				self.password);
 
 		List results = new ArrayList();
-		File startDirectory = new File("C:\\tmp");
+		File startDirectory = new File(props.getProperty("target.dir"));
 		println "Running ..."
 		self.walk(startDirectory, results);
 
