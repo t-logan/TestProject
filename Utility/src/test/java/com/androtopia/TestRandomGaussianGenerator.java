7ab9e22cb552c9a02d37f4729815ecf53d61e925
t-logan <timothy.d.logan@gmail.com>
2014-02-18 21:12:02 -0800

    method name refactor in RandomGaussianGenerator

--- Utility/src/test/java/com/androtopia/TestRandomGaussianGenerator.java
+++ Utility/src/test/java/com/androtopia/TestRandomGaussianGenerator.java
@@ -10,20 +10,20 @@ public class TestRandomGaussianGenerator {
 	@Test
 	public void seededTest() {
 		RandomGaussianGenerator generator = new RandomGaussianGenerator(100, 5);
-		out.println(generator.getNextGaussian());
+		out.println(generator.getNextScaledGaussian());
 	}
 
 	@Test
 	public void unseededTest() {
 		RandomGaussianGenerator generator = new RandomGaussianGenerator();
-		out.println(generator.getNextGaussian(100, 5));
+		out.println(generator.getNextScaledGaussian(100, 5));
 	}
 
 	@Test
 	public void badUnseededTest() {
 		try {
 			RandomGaussianGenerator generator = new RandomGaussianGenerator();
-			out.println(generator.getNextGaussian());
+			out.println(generator.getNextScaledGaussian());
 		} catch (Exception e) {
 			if (!(e instanceof IllegalStateException))
 				Assert.fail();
