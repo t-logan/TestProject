8950d95abaffb6df9508880f1c288b035dc8ab13
t-logan <timothy.d.logan@gmail.com>
2014-03-08 15:07:58 -0800

    able to write image file

--- HDF5/src/main/java/com/hdf5/ImageProcessing.java
+++ HDF5/src/main/java/com/hdf5/ImageProcessing.java
@@ -1,126 +1,60 @@
 package com.hdf5;
 
 import java.awt.image.BufferedImage;
+import java.io.BufferedInputStream;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 
 import javax.imageio.ImageIO;
+import javax.swing.tree.DefaultMutableTreeNode;
 
-import ncsa.hdf.hdf5lib.H5;
-import ncsa.hdf.hdf5lib.HDF5Constants;
 import ncsa.hdf.object.Dataset;
 import ncsa.hdf.object.Datatype;
 import ncsa.hdf.object.FileFormat;
 import ncsa.hdf.object.Group;
 import ncsa.hdf.object.ScalarDS;
 import ncsa.hdf.object.h5.H5Datatype;
+import ncsa.hdf.view.Tools;
 
 public class ImageProcessing {
 
 	private static FileFormat fileFormat;
+	public static final String FILE_TYPE_IMAGE = "IMG";
+
+	private static Tools tools;
 
 	public static void main(String[] args) throws Exception {
-		//createHDFimage();
-		importImage();
-	}
-
-	private static Dataset createHDFimage() throws Exception {
-
-		Dataset dataset = null;
-
-		String name = "FileName.h5";
-
-		int fid = -1, sid = -1, did = -1;
-		long[] dims = { 4, 6 };
-		// Create a new file using default properties.
-		fid = H5.H5Fcreate(name, HDF5Constants.H5F_ACC_TRUNC,
-				HDF5Constants.H5P_DEFAULT, HDF5Constants.H5P_DEFAULT);
-		// Create the data space for the dataset.
-		sid = H5.H5Screate_simple(2, dims, null);
-		// Create the dataset.
-		if ((fid >= 0) && (sid >= 0))
-			did = H5.H5Dcreate(fid, "/dset", HDF5Constants.H5T_STD_I32BE, sid,
-					HDF5Constants.H5P_DEFAULT);
-
-		// Group pgroup = (Group)
-		// groupList.get(parentChoice.getSelectedIndex());
-		// Group pgroup = null;
-		//fileFormat = FileFormat.getFileFormat(FileFormat.FILE_TYPE_HDF5);
-		fileFormat = FileFormat.getInstance(name);
-
-		int w = 10, h = 10;
-
-		// long[] dims = null;
-		int tclass = Datatype.CLASS_CHAR;
-		int tsign = Datatype.SIGN_NONE;
-		int tsize = 1;
-		int torder = Datatype.NATIVE;
-		int interlace = ScalarDS.INTERLACE_PIXEL;
-		int ncomp = 2;
-
-		// HDF5 true color image
-		// if (checkInterlacePixel.isSelected()) {
-		// long[] tmpdims = { h, w, 3 };
-		// dims = tmpdims;
-		// } else {
-		interlace = ScalarDS.INTERLACE_PLANE;
-		long[] tmpdims = { 3, h, w };
-		dims = tmpdims;
-		// }
-
-		int gcpl = 0;
-		Group parent = null;
-		Group pgroup = fileFormat.createGroup("/dset", parent, 0, gcpl);
-		if (pgroup == null)
-			System.out.println("pgroup is NULL");
-
-		try {
-			Datatype datatype = fileFormat.createDatatype(tclass, tsize,
-					torder, tsign);
-			System.out.println(name);
-			dataset = fileFormat.createImage(name, pgroup, datatype, dims,
-					dims, null, -1, ncomp, interlace, null);
-			dataset.init();
-		} catch (Exception ex) {
-			throw ex;
-		} finally {
-			// End access to the dataset and release resources used by it.
-			if (did >= 0)
-				H5.H5Dclose(did);
-			// Terminate access to the data space.
-			if (sid >= 0)
-				H5.H5Sclose(sid);
-			// Close the file.
-			if (fid >= 0)
-				H5.H5Fclose(fid);
-		}
-		return dataset;
+		// createHDFimage();
+		// importImage();
+		saveImageAsHDF("photo116.jpg", "dset.h5", Tools.FILE_TYPE_IMAGE,
+				FileFormat.FILE_TYPE_HDF5);
 	}
 
 	private static void importImage() throws Exception {
-		
+
 		// create a dataset
 		FileFormat ff = FileFormat.getInstance("dset.h5");
 		ff.create("dset.h5");
 		ff.createGroup("ImageGroup", null);
 		ff.close();
 		ff.open();
-		
+
 		int gcpl = 0;
 		Group parent = null;
 		Group pgroup = (Group) ff.get("ImageGroup");
 		String name = "2D image";
-		Datatype dtype = new H5Datatype(Datatype.CLASS_INTEGER, 1, Datatype.NATIVE,
-				Datatype.SIGN_NONE);
+		Datatype dtype = new H5Datatype(Datatype.CLASS_INTEGER, 1,
+				Datatype.NATIVE, Datatype.SIGN_NONE);
 		long[] chunks = null; // no chunking
 		int gzip = 0; // no compression
 		int ncomp = 3; // RGB true color image
 		int interlace = ScalarDS.INTERLACE_PIXEL;
-		
+
 		// read the image file
 		BufferedImage img = null;
 		try {
-		    img = ImageIO.read(new File("photo116.jpg"));
+			img = ImageIO.read(new File("photo0.jpg"));
 		} catch (IOException e) {
 			System.out.println("I/O error when reading the image file");
 		}
@@ -129,29 +63,30 @@ public class ImageProcessing {
 		int[] dataBuffInt = img.getRGB(0, 0, w, h, null, 0, w);
 
 		long[] dims = { h, w };
-		long[] maxdims = dims;	
-		
+		long[] maxdims = dims;
+
 		// create the image dataset
-		Dataset d = ff.createImage(name, pgroup, dtype, dims, maxdims, chunks, gzip, ncomp, interlace, dataBuffInt);
+		Dataset d = ff.createImage(name, pgroup, dtype, dims, maxdims, chunks,
+				gzip, ncomp, interlace, dataBuffInt);
 		ff.close();
 	}
 
-
 	private static void writeAnEmptyImage() throws Exception {
-		
+
 		FileFormat ff = FileFormat.getInstance("dset.h5");
 		ff.create("dset.h5");
 		ff.createGroup("ImageGroup", null);
-		//System.out.println(((DefaultMutableTreeNode) ff.getRootNode().getChildAt(0)).getUserObject());
+		// System.out.println(((DefaultMutableTreeNode)
+		// ff.getRootNode().getChildAt(0)).getUserObject());
 		ff.close();
 		ff.open();
-		
+
 		int gcpl = 0;
 		Group parent = null;
 		Group pgroup = (Group) ff.get("ImageGroup");
 		String name = "2D image";
-		Datatype dtype = new H5Datatype(Datatype.CLASS_INTEGER, 1, Datatype.NATIVE,
-				Datatype.SIGN_NONE);
+		Datatype dtype = new H5Datatype(Datatype.CLASS_INTEGER, 1,
+				Datatype.NATIVE, Datatype.SIGN_NONE);
 		long[] dims = { 100, 100 };
 		long[] maxdims = dims;
 		long[] chunks = null; // no chunking
@@ -159,7 +94,112 @@ public class ImageProcessing {
 		int ncomp = 3; // RGB true color image
 		int interlace = ScalarDS.INTERLACE_PIXEL;
 		Object data = null; // no initial data values
-		Dataset d = ff.createImage(name, pgroup, dtype, dims, maxdims, chunks, gzip, ncomp, interlace, data);
+		Dataset d = ff.createImage(name, pgroup, dtype, dims, maxdims, chunks,
+				gzip, ncomp, interlace, data);
 		ff.close();
+	}
+
+	/**
+	 * Converts an image file into HDF4/5 file.
+	 * 
+	 * @param imgFileName
+	 *            the input image file.
+	 * @param hdfFileName
+	 *            the name of the HDF4/5 file.
+	 * @param fromType
+	 *            the type of image.
+	 * @param toType
+	 *            the type of file converted to.
+	 */
+	public static void saveImageAsHDF(String imgFileName, String hdfFileName,
+			String fromType, String toType) throws Exception {
+		File imgFile = null;
+
+		if (imgFileName == null) {
+			throw new NullPointerException("The source image file is null.");
+		} else if (!(imgFile = new File(imgFileName)).exists()) {
+			throw new NullPointerException(
+					"The source image file does not exist.");
+		} else if (hdfFileName == null) {
+			throw new NullPointerException("The target HDF file is null.");
+		}
+
+		if (!fromType.equals(FILE_TYPE_IMAGE)) {
+			throw new UnsupportedOperationException("Unsupported image type.");
+		} else if (!(toType.equals(FileFormat.FILE_TYPE_HDF4) || toType
+				.equals(FileFormat.FILE_TYPE_HDF5))) {
+			throw new UnsupportedOperationException(
+					"Unsupported destination file type.");
+		}
+
+		BufferedImage image = null;
+		try {
+			BufferedInputStream in = new BufferedInputStream(
+					new FileInputStream(imgFileName));
+			image = ImageIO.read(in);
+			in.close();
+		} catch (Throwable err) {
+			image = null;
+		}
+
+		if (image == null)
+			throw new UnsupportedOperationException("Failed to read image: "
+					+ imgFileName);
+
+		int h = image.getHeight();
+		int w = image.getWidth();
+		byte[] data = null;
+
+		try {
+			data = new byte[3 * h * w];
+		} catch (OutOfMemoryError err) {
+			err.printStackTrace();
+			throw new RuntimeException("Out of memory error.");
+		}
+
+		int idx = 0;
+		int rgb = 0;
+		for (int i = 0; i < h; i++) {
+			for (int j = 0; j < w; j++) {
+				rgb = image.getRGB(j, i);
+				data[idx++] = (byte) (rgb >> 16);
+				data[idx++] = (byte) (rgb >> 8);
+				data[idx++] = (byte) rgb;
+			}
+		}
+
+		long[] dims = null;
+		Datatype type = null;
+		Group pgroup = null;
+		String imgName = imgFile.getName();
+		FileFormat newfile = null, thefile = null;
+		if (toType.equals(FileFormat.FILE_TYPE_HDF5)) {
+			thefile = FileFormat.getFileFormat(FileFormat.FILE_TYPE_HDF5);
+			long[] h5dims = { h, w, 3 }; // RGB pixel interlace
+			dims = h5dims;
+		} else if (toType.equals(FileFormat.FILE_TYPE_HDF4)) {
+			thefile = FileFormat.getFileFormat(FileFormat.FILE_TYPE_HDF4);
+			long[] h4dims = { w, h, 3 }; // RGB pixel interlace
+			dims = h4dims;
+		} else {
+			thefile = null;
+		}
+
+		if (thefile != null) {
+			newfile = thefile.createInstance(hdfFileName, FileFormat.CREATE);
+			newfile.open();
+			pgroup = (Group) ((DefaultMutableTreeNode) newfile.getRootNode())
+					.getUserObject();
+			type = newfile.createDatatype(Datatype.CLASS_CHAR, 1,
+					Datatype.NATIVE, Datatype.SIGN_NONE);
+			newfile.createImage(imgName, pgroup, type, dims, null, null, -1, 3,
+					ScalarDS.INTERLACE_PIXEL, data);
+			newfile.close();
+		}
+
+		// clean up memory
+		data = null;
+		image = null;
+		Runtime.getRuntime().gc();
 	}
 }
